3f7f8808850763339629b9fb76c682ad319385a3
Lucas Hergessel <lucas.hergessel@gmail.com>
2025-08-16 12:03:39 -0300

    creates validation flow and adds containsduplicate code

---
+++ LukeCode/ArraysAndHashing/ContainsDuplicate.cs
@@ -0,0 +1,34 @@
+﻿namespace LukeCode.ArraysAndHashing;
+public static class ContainsDuplicate
+{
+    private static int[] _sample1 = new[] { 1, 2, 3, 4, 4, 5 };
+    private static int[] _sample2 = new[] { 1, 2, 3, 4, 5 };
+    public static bool RunReturningIfCodeIsCorrect()
+    {
+        Dictionary<int[], bool> samples = new()
+        {
+            { _sample1, true },
+            { _sample2, false }
+        };
+
+        foreach (var sample in samples)
+        {
+            if (HasDuplicate(sample.Key) != sample.Value)
+                return false;
+        }
+        return true;
+    }
+
+    public static bool HasDuplicate(int[] nums)
+    {
+        var set = new HashSet<int>();
+
+        foreach (int num in nums)
+        {
+            if (set.Contains(num))
+                return true;
+            set.Add(num);
+        }
+        return false;
+    }
+}
--- LukeCode/Program.cs
+++ LukeCode/Program.cs
@@ -1,2 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LukeCode.ArraysAndHashing;
+
+Dictionary<int,string> categories = new()
+{
+    { 1, "Arrays & Hashing" },
+};
+
+// Printing category choices
+Console.WriteLine("Choose category typing the related number:");
+foreach (var category in categories)
+{
+    string message = string.Concat(category.Key, " for ", category.Value);
+    Console.WriteLine(message);
+}
+
+
+var valid = false;
+int chosenCategory = 0;
+
+// Getting user category response
+while (!valid)
+{
+    // Reads user response
+    var categoryResponse = Console.ReadLine();
+    
+    if (categoryResponse is null)
+    {
+        RespondError();
+        break;
+    }
+
+    if (int.TryParse(categoryResponse, out var result))
+        chosenCategory = result;
+
+    if (categories.Keys.Contains(chosenCategory))
+        valid = true;
+    else
+    {
+        RespondError();
+        break;
+    }
+}
+
+switch (chosenCategory)
+{
+    case 1: ContainsDuplicate.RunReturningIfCodeIsCorrect();
+        break;
+
+    _: ContainsDuplicate.RunReturningIfCodeIsCorrect();
+}
+void RespondError()
+{
+    Console.WriteLine("Type a valid number, options being:");
+    foreach (var category in categories)
+    {
+        var message = string.Concat(category.Key, " - ", category.Value);
+        Console.WriteLine(message);
+    }
+}
