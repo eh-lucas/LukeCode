3b874e94a3081730966f360a2e6c8e9aae4aa846
eh-lucas <lucas.hergessel@gmai.com>
2025-09-01 06:19:33 -0300

    adding topkfrequent solution

---
+++ LukeCode/ArraysAndHashing/TopKFrequent.cs
@@ -0,0 +1,36 @@
+namespace LukeCode.ArraysAndHashing;
+public static class TopKFrequent 
+{
+    public static int[] TopKFrequent(int[] nums, int k)
+    {
+	    var count = new Dictionary<int, int>();
+	    var freq = new List<int>[nums.Length + 1];
+	    
+	    for (int i = 0; i < freq.Length; i++){
+		    freq[i] = new List<int>();
+	    }
+	   
+	    foreach (var n in nums){
+		    if (!count.ContainsKey(n))
+			    count.Add(n, 0);
+		    count[n]++;
+	    }
+
+	    foreach (var entry in count){
+		    freq[entry.Value].Add(entry.Key);
+	    }
+	    
+	    int[] res = new int[k];
+	    int index = 0;
+	    for (int i = freq.Length - 1; i > 0 && index < k; i--)
+	    {
+		    foreach (int n in freq[i]){
+			   res[index] = n;
+			   index++;
+			   if (index == k) return res;
+		    }
+	    }
+
+	    return res;
+    }
+}
